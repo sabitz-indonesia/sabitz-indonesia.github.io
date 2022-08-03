caba42b539879ae34fae6f9db321f222c3e20378
Moh Affan <b4affan@gmail.com>
2022-08-03 16:17:14 +0700

    loader moon

--- assets/js/main.js
+++ assets/js/main.js
@@ -483,7 +483,7 @@ function initMap() {
   //--------------------- Preloader ----------------------
   if (preloader.enabled == true) {
     window.addEventListener('load', function () {
-      $('.preloaderWrapper').delay(300).fadeOut();
+      $('.preloaderWrapper').delay(6000).fadeOut();
       $('body').addClass('animRDY');
     });
   }
